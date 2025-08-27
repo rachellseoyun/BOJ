a403ebf8c5fd93c27671f581100a0721d81f7781
rachellseoyun <rachellseoyun@users.noreply.github.com>
2025-08-27 14:17:41 +0900

    1000.c source code

--- Exercise_by_step/1_입출력과 사칙연산/1000_A+B/1000.c
+++ Exercise_by_step/1_입출력과 사칙연산/1000_A+B/1000.c
@@ -2,4 +2,12 @@
 // Created by rache on 25. 8. 27..
 //
 
-// not yet
+#include <stdio.h>
+
+int main(void) {
+    int A, B;
+    if (scanf("%d %d", &A, &B) == 2) {
+        printf("%d\n", A + B);
+    }
+    return 0;
+}
