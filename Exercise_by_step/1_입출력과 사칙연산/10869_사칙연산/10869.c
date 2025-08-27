5438a0a59ce8c619093fd8088095a828e6eb4854
rachellseoyun <rachellseoyun@users.noreply.github.com>
2025-08-27 16:10:07 +0900

    10869.c source code

---
+++ Exercise_by_step/1_입출력과 사칙연산/10869_사칙연산/10869.c
@@ -0,0 +1,15 @@
+//
+// Created by rache on 25. 8. 27..
+//
+#include <stdio.h>
+
+int main(void) {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d\n", A + B);
+    printf("%d\n", A - B);
+    printf("%d\n", A * B);
+    printf("%d\n", A / B);
+    printf("%d\n", A % B);
+    return 0;
+}
